2644623270ce4ed08d8421f676bbdc04ba209f2c
ariel <95013498+matbuha@users.noreply.github.com>
2024-01-09 14:35:56 +0200

    creating functionality to the app

---
+++ Assets/Scripts/ChecklistManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChecklistManager : MonoBehaviour {
+
+    // Public variables for linking Unity objects and UI elements
+    public Transform content; // Parent transform for checklist items
+    public GameObject addPanel; // Panel for adding new checklist items
+    public Button createButton; // Button to create a new checklist item
+    public GameObject checklistItemPrefab; // Prefab for checklist items
+
+    string filePath; // Path for saving checklist data
+
+    // A list to keep track of checklist items
+    private List<ChecklistObject> checklistObjects = new List<ChecklistObject>();
+
+    // Array for storing input fields from the addPanel
+    private InputField[] addInputFields;
+
+    // Start is called before the first frame update
+    private void Start() {
+        // Set the file path for saving checklist data
+        filePath = Application.persistentDataPath + "/checklist.txt";
+
+        // Get all InputField components from addPanel
+        addInputFields = addPanel.GetComponentsInChildren<InputField>();
+
+        // Add a listener to the createButton to call CreateChecklistItem when clicked
+        if (addInputFields != null && addInputFields.Length >= 2) {
+            createButton.onClick.AddListener(delegate {CreateChecklistItem(addInputFields[0].text, addInputFields[1].text); });
+        } else {
+            Debug.LogError("Not enough input fields found in addPanel.");
+        }
+    }
+
+    // Method to switch between different UI modes
+    public void SwitchMode(int mode) {
+        switch (mode) {
+            // Mode 0: Regular checklist view
+            case 0:
+                addPanel.SetActive(false); // Hide the addPanel
+                break;
+            
+            // Mode 1: Adding a new checklist item
+            case 1:
+                addPanel.SetActive(true); // Show the addPanel
+                break;
+        }
+    }
+
+    // Method to create a new checklist item
+    void CreateChecklistItem(string name, string type) {
+        // Create a new checklist item from the prefab
+        GameObject item = Instantiate(checklistItemPrefab);
+
+        // Set the parent of the new item to 'content'
+        item.transform.SetParent(content);
+
+        // Get the ChecklistObject component of the new item
+        ChecklistObject itemObject = item.GetComponent<ChecklistObject>();
+
+        // Determine the index for the new item
+        int index = 0;
+        if(checklistObjects.Count > 0) {
+            index = checklistObjects.Count - 1;
+        }
+        
+        // Set the details of the new checklist item
+        itemObject.SetObjectInfo(name, type, index);
+
+        // Add the new item to the list of checklist items
+        checklistObjects.Add(itemObject);
+
+        // Temporarily store the itemObject
+        ChecklistObject temp = itemObject;
+
+        // Add a listener to handle changes in the checklist item's toggle state
+        itemObject.GetComponent<Toggle>().onValueChanged.AddListener(delegate {CheckItem(temp); });
+
+        // Switch back to the regular checklist view
+        SwitchMode(0);
+    }
+
+    // Method to handle checklist item changes
+    void CheckItem(ChecklistObject item) {
+        // Remove the item from the list of checklist items
+        checklistObjects.Remove(item);
+    }
+}
