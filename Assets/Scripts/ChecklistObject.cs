3178376e4f3075033bd7459b42abc976d66b994b
ariel <95013498+matbuha@users.noreply.github.com>
2024-01-10 23:01:47 +0200

    Creating view to Title and content inside the main page
    - Fixing the Last Task view, now it will be whole content

--- Assets/Scripts/ChecklistObject.cs
+++ Assets/Scripts/ChecklistObject.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ChecklistObject : MonoBehaviour {
-
-    public string objName;
-    public string type;
+    public string objName; // Title of the task
+    public string type; // Content of the task
     public int index;
 
-    private Text itemText;
+    private Text titleText; // Text component for the title
+    private Text contentText; // Text component for the content
 
     private void Start() {
-        itemText = GetComponentInChildren<Text>();
-        itemText.text = objName;
+        // Assuming you have two Text children: one for the title and one for the content
+        Text[] textComponents = GetComponentsInChildren<Text>();
+        if(textComponents.Length >= 2) {
+            titleText = textComponents[0]; // First Text component for the title
+            contentText = textComponents[1]; // Second Text component for the content
+        }
+
+        UpdateItemText();
     }
 
     public void SetObjectInfo(string name, string content, int index) {
         objName = name;
         type = content;
         this.index = index;
+
+        UpdateItemText();
+    }
+
+    private void UpdateItemText() {
+        if (titleText != null && contentText != null) {
+            titleText.text = objName; // Set the title
+            contentText.text = type; // Set the content
+        }
     }
 }
